71fb3110746bfdc0e01f732c8fdf30d182a704a0
marchuanv <marchuanv@gmail.com>
2020-06-11 01:37:44 +0200

    refactor

--- component.request.handler.user.js
+++ component.request.handler.user.js
@@ -1,5 +1,6 @@
 const requestHandler = require("component.request.handler.deferred");
 const delegate = require("component.delegate");
+const fs = require("fs");
 const logging = require("logging");
 const utils = require("utils");
 logging.config.add("Request Handler User");
@@ -8,14 +9,24 @@ module.exports = {
     handle: ({ callingModule, port, path }) => {
         delegate.register(thisModule, async (request) => {
             let results = { headers: {}, statusCode: -1, statusMessage: "" };
-            let { username, passphrase } = request.headers;
-            if (!username){
-                request.headers.username = "anonymous";
-            }
-            if (!passphrase){
-                const { hashedPassphrase, salt } = utils.hashPassphrase("anonymous");
-                request.headers.passphrase = hashedPassphrase;
-                request.headers.salt = salt;
+            let { username, passphrase, fromhost, fromport } = request.headers;
+            // if (!username){
+            //     request.headers.username = "anonymous";
+            // }
+            // if (!passphrase){
+            //     const { hashedPassphrase, salt } = utils.hashPassphrase("anonymous");
+            //     request.headers.passphrase = hashedPassphrase;
+            //     request.headers.salt = salt;
+            // }
+            if(!fromhost || !fromport || !username || !passphrase ){
+                results.statusCode = 200;
+                results.statusMessage = "success";
+                results.contentType = "text/html";
+                const htmlTemplate = fs.readFileSync(`${__dirname}/user.html`,"utf8");
+                results.data = htmlTemplate.replace("[path]", request.path);
+                return results;
+            } else {
+
             }
             return await delegate.call(callingModule, request);
         });
