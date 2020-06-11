d5bcc1af61c8b41c6a9dda4bf9f064cf2bd13b6f
marchuanv <marchuanv@gmail.com>
2020-06-12 00:33:45 +0200

    refactor

--- component.request.handler.user.js
+++ component.request.handler.user.js
@@ -10,18 +10,25 @@ module.exports = {
         delegate.register(thisModule, async (request) => {
             let results = { headers: {}, statusCode: -1, statusMessage: "" };
             let { username, passphrase, fromhost, fromport } = request.headers;
-            if(!fromhost || !fromport || !username || !passphrase ){
+            if( fromhost && !isNaN(fromport) && username && passphrase ) {
+                const { hashedPassphrase, salt } = utils.hashPassphrase(passphrase);
+                request.headers.passphrase = hashedPassphrase;
+                request.headers.salt = salt;
+                request.headers.fromport = Number(fromport);
+                return await delegate.call(callingModule, request);
+            } else if ( fromhost && username && passphrase && isNaN(fromport)){
+                results.statusCode = 400;
+                results.statusMessage = "fromport is not a number";
+                results.contentType = "text/plain";
+                results.data = results.statusMessage;
+                return results;
+            } else {
                 results.statusCode = 200;
                 results.statusMessage = "success";
                 results.contentType = "text/html";
                 const htmlTemplate = fs.readFileSync(`${__dirname}/user.html`,"utf8");
                 results.data = htmlTemplate.replace("[path]", request.path);
                 return results;
-            } else {
-                const { hashedPassphrase, salt } = utils.hashPassphrase(passphrase);
-                request.headers.passphrase = hashedPassphrase;
-                request.headers.salt = salt;
-                return await delegate.call(callingModule, request);
             }
         });
         requestHandler.handle({ callingModule: thisModule, port, path });
