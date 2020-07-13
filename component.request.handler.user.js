d206a56bcfe12e04581e576bf348aa75866302f0
marchuanv <marchuanv@gmail.com>
2020-07-13 20:09:21 +0200

    fixed

--- component.request.handler.user.js
+++ component.request.handler.user.js
@@ -7,27 +7,27 @@ module.exports = {
     handle: (callingModule, options = {} ) => {
         delegate.register(thisModule, async (request) => {
             let results = { headers: {}, statusCode: -1, statusMessage: "" };
-            let { username, passphrase, hashedpassphrase, hashedpassphrasesalt, fromHost, fromPort } = request.headers;
-            if ( username && fromHost && !isNaN(fromPort) && ( passphrase || ( hashedPassphrase && hashedPassphraseSalt) ) ) {
-                request.headers.fromHost = fromHost;
-                request.headers.fromPort = fromPort;
+            let { username, passphrase, hashedpassphrase, hashedpassphrasesalt, fromhost, fromport } = request.headers;
+            if ( username && fromhost && !isNaN(fromport) && ( passphrase || ( hashedpassphrase && hashedpassphrasesalt) ) ) {
+                request.headers.fromhost = fromhost;
+                request.headers.fromport = fromport;
                 request.headers.username = username;
                 request.headers.passphrase = passphrase;
-                request.headers.hashedPassphrase = hashedpassphrase;
-                request.headers.hashedPassphraseSalt = hashedpassphrasesalt;
+                request.headers.hashedpassphrase = hashedpassphrase;
+                request.headers.hashedpassphrasesalt = hashedpassphrasesalt;
                 return await delegate.call(callingModule, request);
-            } else if ( options.username && options.fromHost && !isNaN(options.fromPort) && options.hashedPassphrase && options.hashedPassphraseSalt ) {
-                request.headers.fromHost = options.fromHost;
-                request.headers.fromPort = options.fromPort;
+            } else if ( options.username && options.fromhost && !isNaN(options.fromport) && options.hashedpassphrase && options.hashedpassphrasesalt ) {
+                request.headers.fromhost = options.fromhost;
+                request.headers.fromport = options.fromport;
                 request.headers.username = options.username;
-                request.headers.hashedPassphrase = options.hashedPassphrase;
-                request.headers.hashedPassphraseSalt = options.hashedPassphraseSalt;
+                request.headers.hashedpassphrase = options.hashedpassphrase;
+                request.headers.hashedpassphrasesalt = options.hashedpassphrasesalt;
                 return await delegate.call(callingModule, request);
             } else {
                 results.statusCode = 400;
                 results.statusMessage = "Bad Request";
                 results.contentType = "text/plain";
-                results.data = "missing headers: username, fromPort, fromHost and passphrase ";
+                results.data = "missing headers: username, fromport, fromhost and passphrase ";
                 return results;
             }
         });
