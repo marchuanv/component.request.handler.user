5409315fad864bc5e14abcd3b0c2b0588e99f9b4
marchuanv <marchuanv@gmail.com>
2020-07-13 22:55:43 +0200

    fixed

--- component.request.handler.user.js
+++ component.request.handler.user.js
@@ -16,7 +16,7 @@ module.exports = {
                 request.headers.hashedpassphrasesalt = hashedpassphrasesalt;
                 return await delegate.call(callingModule, request);
             } else {
-                const message = "missing headers: username, fromport, fromhost and passphrase";
+                const message = "missing headers: username, fromport or fromhost";
                 return { 
                     headers: { 
                         "Content-Type":"text/plain", 
