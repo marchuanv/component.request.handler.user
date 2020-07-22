290d631194239c6bd1c99a7267db26ea98249ec0
marchuanv <marchuanv@gmail.com>
2020-07-22 12:13:28 +0200

    refactor

--- component.request.handler.user.js
+++ component.request.handler.user.js
@@ -12,7 +12,7 @@ module.exports = {
                     request.headers.fromhost = fromhost;
                     request.headers.fromport = Number(fromport);
                     request.headers.username = username;
-                    return await delegate.call({ context: "component.request.handler.secure.authenticate" }, request);
+                    return await delegate.call({ context: `component.request.handler.secure.authenticate.${request.path}` }, request);
                 } else {
                     return { 
                         headers: { "Content-Type":"text/plain" },
