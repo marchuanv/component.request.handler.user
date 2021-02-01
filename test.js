81df50a6d8da74398c4b7d57dfefc038386964ff
marchuanv <marchuanv@gmail.com>
2021-02-01 14:36:25 +0200

    refactor

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 const requestHandler = require("./component.request.handler.user.js");
+const request = require("component.request");
 const delegate = require("component.delegate");
 (async()=>{ 
     delegate.register("component.request.handler.secure.authenticate", "3000/test", () => {
-        return { statusCode: 200, statusMessage: "Success", headers: {}, data: null };
-    });
-    delegate.register("component.request.handler.secure.authenticate", "3000/authenticate", () => {
         return { statusCode: 200, statusMessage: "Success", headers: {}, data: null };
     });
     await requestHandler.handle({
@@ -12,11 +10,7 @@ const delegate = require("component.delegate");
         host: "localhost",
         port: 3000
     });
-    await requestHandler.handle({
-        path: "/authenticate",
-        host: "localhost",
-        port: 3000
-    });
+    await request.send({ host: "localhost", port: 3000, path: "/test", method: "GET", headers: {username: "marchuanv", fromhost: "localhost", fromport: 6000 }, data: "", retryCount: 1  });
 })().catch((err)=>{
     console.error(err);
 });
