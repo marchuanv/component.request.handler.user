a82a469908eed66cdfca034c1c4aa43e9e11faf2
marchuanv <marchuanv@gmail.com>
2021-02-23 02:30:08 +0200

    refactor

--- component.request.handler.user.js
+++ component.request.handler.user.js
@@ -1,18 +1,55 @@
 const requestHandlerDeferred = require("component.request.handler.deferred");
 const delegate = require("component.delegate");
 const logging = require("logging");
+const utils = require("utils");
 logging.config.add("Request Handler User");
+const userSessions = [];
 module.exports = { 
     handle: (options ) => {
         requestHandlerDeferred.handle(options);
         const name = `${options.port}${options.path}`;
         delegate.register(`component.request.handler.user`, name, async (request) => {
-            let { username, fromhost, fromport } = request.headers;
-            if ( username && fromhost && !isNaN(fromport)) {
-                request.headers.fromhost = fromhost;
-                request.headers.fromport = Number(fromport);
-                request.headers.username = username;
-                return await delegate.call({ context: `component.request.handler.secure.authenticate`, name }, request);
+            
+            let { username, fromhost, fromport, sessionid } = request.headers;
+            
+            delete request.headers["username"];
+            delete request.headers["fromhost"];
+            delete request.headers["fromport"];
+            delete request.headers["sessionid"];
+
+            let userSession = userSessions.find(s => s.Id === sessionid);
+            if (userSession){
+                const results = await delegate.call({ context: `component.request.handler.secure.authenticate`, name }, {
+                    session: userSession,
+                    headers: request.headers
+                });
+                results.headers.sessionid = userSession.Id;
+                return results;
+            }
+            
+            userSession = userSessions.find(s => s.username === username);
+            if (userSession) {
+                return { 
+                    headers: { "Content-Type":"text/plain" },
+                    statusCode: 400,
+                    statusMessage: "Bad Request",
+                    data: "missing sessionid in request header"
+                };
+            }
+            if (username && fromhost && !isNaN(fromport)) {
+                userSession = { 
+                    Id: utils.generateGUID(),
+                    fromhost,
+                    fromport: Number(fromport),
+                    username
+                };
+                userSessions.push(userSession);
+                const results = await delegate.call({ context: `component.request.handler.secure.authenticate`, name }, {
+                    session: userSession,
+                    headers: request.headers
+                });
+                results.headers.sessionid = userSession.Id;
+                return results;
             } else {
                 return { 
                     headers: { "Content-Type":"text/plain" },
--- test.js
+++ test.js
@@ -1,31 +1,64 @@
 const requestHandler = require("./component.request.handler.user.js");
-const request = require("component.request");
+const unsecureRequest = require("component.request.unsecure");
 const delegate = require("component.delegate");
 (async()=>{ 
-    delegate.register("component.request.handler.secure.authenticate", "3000/test", () => {
-        return { statusCode: 200, statusMessage: "Success", headers: {}, data: null };
+    const newUnsecureRequest = { host: "localhost", port: 3000, path: "/test" };
+    delegate.register("component.request.handler.secure.authenticate", `${newUnsecureRequest.port}${newUnsecureRequest.path}`, (data) => {
+        return { statusCode: 200, statusMessage: "Test Passed", headers: {}, data: "Test Passed" };
     });
-    await requestHandler.handle({
-        path: "/test",
-        host: "localhost",
-        port: 3000
-    });
-    const results = await request.send({ 
-        host: "localhost", 
-        port: 3000, 
-        path: "/test", 
-        method: "GET", 
+    await requestHandler.handle(newUnsecureRequest);
+
+    let results = await unsecureRequest.send({
+        host: newUnsecureRequest.host,
+        port: newUnsecureRequest.port,
+        path: newUnsecureRequest.path,
+        method: "GET",
         headers: {
-            username: "marchuanv", 
-            fromhost: "localhost", 
+            username: "marchuanv",
+            fromhost: "localhost",
             fromport: 6000
-        }, 
-        data: "", 
-        retryCount: 1 
+        },
+        data: ""
     });
-    if (results.statusCode !== 200){
+    if (results.statusCode !== 200 || results.statusMessage !== "Test Passed"){
         throw new Error("user identification test failed for port 3000");
     }
+   
+    results = await unsecureRequest.send({
+        host: newUnsecureRequest.host,
+        port: newUnsecureRequest.port,
+        path: newUnsecureRequest.path,
+        method: "GET",
+        headers: {
+            username: "marchuanv",
+            fromhost: "localhost",
+            fromport: 6000,
+            sessionid: results.headers.sessionid
+        },
+        data: ""
+    });
+    if (results.statusCode !== 200 || results.statusMessage !== "Test Passed"){
+        throw new Error("user identification test failed for port 3000");
+    }
+    
+    results = await unsecureRequest.send({
+        host: newUnsecureRequest.host,
+        port: newUnsecureRequest.port,
+        path: newUnsecureRequest.path,
+        method: "GET",
+        headers: {
+            username: "marchuanv",
+            fromhost: "localhost",
+            fromport: 6000
+        },
+        data: ""
+    });
+    if (results.statusCode !== 400 || results.statusMessage !== "Bad Request"){
+        throw new Error("user identification test failed for port 3000");
+    }
+
+    process.exit();
+
 })().catch((err)=>{
     console.error(err);
 });
