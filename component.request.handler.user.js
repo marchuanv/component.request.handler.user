be4da978324e48894600300a95adc9daa68a5567
marchuanv <marchuanv@gmail.com>
2021-03-24 12:33:47 +0200

    refactor

--- component.request.handler.user.js
+++ component.request.handler.user.js
@@ -8,32 +8,27 @@ component.register({ moduleName: "component.request.handler.user" }).then( async
     const { routes, port } = config.requestHandlerRoute;
     for(const route of routes){
         const name = `${port}${route.path}`;
-        requestHandlerUser.subscribe( { name }, async (request) => {
+        const ensureSession = async (request) => {
             let { username, fromhost, fromport, sessionid } = request.headers;
             
             delete request.headers["username"];
             delete request.headers["fromhost"];
             delete request.headers["fromport"];
             delete request.headers["sessionid"];
-        
-            let userSession = userSessions.find(s => s.Id === sessionid);
-            if (userSession){
-                await requestHandlerUser.log(`${userSession.Id} Id found for ${userSession.username}`);
-                return await requestHandlerUser.publish({ name }, {
-                    session: userSession,
-                    headers: request.headers,
-                    data: request.data
-                });
-            }
-            
-            if (userSessions.filter(s => s.username === username).length > 1){
-                await requestHandlerUser.log(`ending older sessions, more than one session found for ${userSession.username}`);
-                const sortedUserSessions = userSession.sort((a, b) => b.date - a.date);
-                userSessions = userSessions.filter(s => s.username === username && s.Id !==  sortedUserSessions[0].Id);
+
+            if (!username && sessionid){
+                username = userSessions.find(s => s.Id === sessionid);
             }
 
-            userSession = userSessions[0];
-            if (userSession) {
+            const hasMultipleSessions = userSessions.filter(s => s.username === username).length > 1;
+            if (hasMultipleSessions){
+                await requestHandlerUser.log(`ending older sessions, more than one session found for ${username}`);
+                const latestedSession = userSession.filter(s => s.username === username).sort((a, b) => b.date - a.date)[0];
+                userSessions = userSessions.filter(s => s.username === username && s.Id !==  latestedSession.Id);
+            }
+        
+            let userSession = userSessions.find(s => s.username === username); //should only be one session after clearing
+            if (userSession){
                 await requestHandlerUser.log(`${userSession.Id} Id found for ${userSession.username}`);
                 const results = await requestHandlerUser.publish({ name }, {
                     session: userSession,
@@ -45,8 +40,9 @@ component.register({ moduleName: "component.request.handler.user" }).then( async
                 }
                 return results;
             }
+
             if (username && fromhost && !isNaN(fromport)) {
-                userSession = { 
+                userSession = {
                     Id: utils.generateGUID(),
                     fromhost,
                     fromport: Number(fromport),
@@ -55,23 +51,16 @@ component.register({ moduleName: "component.request.handler.user" }).then( async
                 };
                 userSessions.push(userSession);
                 await requestHandlerUser.log(`session created for ${username}`);
-                const results = await requestHandlerUser.publish({ name }, {
-                    session: userSession,
-                    headers: request.headers,
-                    data: request.data
-                });
-                if (results && results.headers){
-                    results.headers.sessionid = userSession.Id;
-                }
-                return results;
-            } else {
-                return { 
-                    headers: { "Content-Type":"text/plain" },
-                    statusCode: 400,
-                    statusMessage: "Bad Request",
-                    data: "missing headers: username, fromport and fromhost"
-                };
+                return await ensureSession(request);
             }
-        });
+
+            return { 
+                headers: { "Content-Type":"text/plain" },
+                statusCode: 400,
+                statusMessage: "Bad Request",
+                data: "failed to create session, make sure the { username, fromport, fromhost } headers are present."
+            };
+        };
+        requestHandlerUser.subscribe( { name }, ensureSession );
     };
 });
